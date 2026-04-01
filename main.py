17b5c1b006b75f1b8d0046293dfc07b8b2620b6a
yueran-i <t2284358620@gmail.com>
2026-04-02 00:34:56 +0800

    新增百度搜索爬虫代码main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print 
+import requests
+params = {"wd": "python"}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/123.0.0.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
+    "Referer": "https://www.baidu.com/", 
+    "Connection": "keep-alive" 
+}
+response = requests.get("https://www.baidu.com/s",params=params,headers=headers)
+print("最终请求的网址：",response.url)
+response.encoding = "utf-8"
+print("输出的内容",response.text)
