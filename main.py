e63793ae3703318260db8c02cf67f999d135b4af
yueran-i <t2284358620@gmail.com>
2026-04-02 23:05:38 +0800

    使输出不再直接显示html，显示标题，链接

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 params = {"wd": "python"}
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/123.0.0.0",
@@ -9,4 +10,15 @@ headers = {
 response = requests.get("https://www.baidu.com/s",params=params,headers=headers)
 print("最终请求的网址：",response.url)
 response.encoding = "utf-8"
-print("输出的内容",response.text)
+htlm = response.text
+soup = BeautifulSoup(htlm,"html.parser")
+results = soup.find_all("div",class_="result")
+for result in results:
+    title_tag = result.find("h3")
+    if title_tag :
+        title = title_tag.get_text()
+    a_tag = result.find("a")
+    if a_tag :
+        link = a_tag["href"]
+        print("标题：",title)
+        print("链接：",link)
